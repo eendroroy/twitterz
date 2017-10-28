f03e33019a3b8efe6672fd713546cdcaa5bc6d2f
indrajit <eendroroy@gmail.com>
2017-10-28 19:18:50 +0600

    initial commit

---
+++ src/main/java/com/github/eendroroy/twitterz/TwitterzApplication.java
@@ -0,0 +1,12 @@
+package com.github.eendroroy.twitterz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterzApplication.class, args);
+	}
+}
