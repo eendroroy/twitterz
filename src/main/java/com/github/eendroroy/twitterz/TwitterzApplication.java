2c1cf4fa2106786b7529b16ca44145e2359a25a4
indrajit <eendroroy@gmail.com>
2017-10-31 22:49:17 +0600

    enable autoconfiguration

--- src/main/java/com/github/eendroroy/twitterz/TwitterzApplication.java
+++ src/main/java/com/github/eendroroy/twitterz/TwitterzApplication.java
@@ -1,12 +1,14 @@
 package com.github.eendroroy.twitterz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author indrajit
  */
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class TwitterzApplication {
 
